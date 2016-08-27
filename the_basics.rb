c157d799b471feb5e5b22afc412d4825ce0783b6
Shannon Renninger <shannonrenn@gmail.com>
2016-08-27 16:05:49 -0400

    the_basics exercises complete

---
+++ the_basics.rb
@@ -0,0 +1,17 @@
+movies = {movie1: 2000,
+          movie2: 2001,
+          movie3: 2002}
+puts movies [:movie1]
+puts movies [:movie2]
+puts movies [:movie3]
+dates = [2000, 2001, 2002]
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 1.5**2
+puts 2.5**2
+puts 3.5**2
